03d474aadcb8532e28c146702d6b11a593e9abef
askobeldin <askobeldin@gmail.com>
2016-08-02 17:35:54 +0300

    added headers

---
+++ regexps1/experiments/data/headers.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+################################################################################
+# looking for \n for different platforms --> (?:\n|\r|\r\n?)
+
+
+
+h1 = '# This is header one'
+h11 = 'This is header one.   \n==============='
+h12 = h1 + '   #  '
+h13 = '#   Test-driven development'
+
+h2 = '## This is header two'
+h21 = 'This is header two\n---------------'
+
+
+# main table for testing
+# must be in every file
+# third element in tuple - flags for comile: M, I, S
+# in case of some flags = 'MIS'
+retable = (
+    (r'#', h1),
+    (r'#{1}\s', h1),
+    (r'#{1}\s+(?P<text>\w+)', h1),
+    (r'#{1}\s+(?P<text>.+)', h1),
+    (r'#{1}\s+(?P<text>.+)', h1 + '  #    '),
+    (r'#{1}\s+(?P<text>.+?)', h1 + '  #    '),
+    (r'#{1}\s+(?P<text>.+\b)', h12),
+    (r'#{1}\s+(?P<text>.+\b)', h13),
+    (r'#{1}\s+(?P<text>.+\b)', h13 + '#'),
+    (r'#{1}\s?(?P<text>.+\b)', '# Tesing header. #  '),
+    (r'#{1}\s?(?P<text>.+?\b)', '# Tesing header. #  '),
+    (r'#{1}\s?(?P<text>.+\b)', '#Tesing header.#  '),
+    (r'#{1}\s?(?P<text>.+\b)', '# Tesing header 34. #  '),   # good
+
+    (r'(?P<text>\b.+\b)[\W\s]+(?:\n|\r|\r\n?)\=+', h11, 'M'),  # good
+    (r'(?P<text>\b.+\b)[\W\s]+(?:\n|\r|\r\n?)\=+', h11 + '   ', 'M'),
+    (r'(?P<text>\b.+\b)[\W\s]+(?:\n|\r|\r\n?)\=+', '3. ' + h11 + '   ', 'M'),
+
+    (r'^[^\d\s\.](?P<text>\b.+\b)[\W\s]+(?:\n|\r|\r\n?)\=+', '3. ' + h11 + '   ', 'M'),
+)
+
