1ed2d0dbfdeb626e320ee736aca6bd4528e4553e
askobeldin <askobeldin@gmail.com>
2016-08-15 09:42:25 +0300

    little changes

--- mymarkdown/iterreader2.py
+++ mymarkdown/iterreader2.py
@@ -15,12 +15,12 @@ from collections import namedtuple
 TOKENS_TABLE = (
         ('HEADER1', r'^#{1,6}\s?.+(?:\n|\r|\r\n?){2}(?im)'),
         ('HEADER2', r'^.+\s*(?:\n|\r|\r\n?)[\=|\-]{5,}(?:\n|\r|\r\n?){2}(?im)'),
-        # ('EMPTYLINE', r'^\s*$(?m)'),
         ('UNORDEREDLISTITEM', r'^\s*(\-|\+|\*)\s+(.+(?:\n|\r|\r\n?))(?im)'),
         ('ORDEREDLISTITEM', r'^\s*\d+\.\d*\s+(.+(?:\n|\r|\r\n?))(?im)'),
         ('BLOCKQUOTEITEM', r'^\s*\>\s*?(.+(?:\n|\r|\r\n?))(?im)'),
         # it must be last; it can match empty text line
         ('TEXTITEM', r'(.+(?:\n|\r|\r\n?))+?(?im)'),
+        ('EMPTYLINE', r'^\s*$(?m)'),
 )
 
 TOK_REGEX = '|'.join(['(?P<%s>%s)' % pair for pair in TOKENS_TABLE])
@@ -37,24 +37,25 @@ def textblocks(txt):
         yield BLOCK(kind, value)
 
 
-def main(datafile):
+def main(arguments):
     n = itertools.count(start=1)
     fmt1 = '[I] block: {block:0>3}, type: {type}\n{text}'
-    with open(datafile, 'r', encoding='utf-8') as f:
+    with open(arguments.datafile, 'r', encoding='utf-8') as f:
         textlines = f.readlines()
     for block in textblocks(''.join(textlines)):
-        # print(fmt1.format(block=next(n),
-                          # type=block.type,
-                          # text='{!r}'.format(block.value)))
+        if arguments.verbose:
+            text = '{!r}'.format(block.value)
+        else:
+            text = block.value
         print(fmt1.format(block=next(n),
                           type=block.type,
-                          text=block.value))
-    # debugging
-    # print('\n\ntxt is:\n{!r}'.format(txt))
+                          text=text))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('datafile', help="Text file to read")
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="Increase output verbosity")
     arguments = parser.parse_args(sys.argv[1:])
-    main(arguments.datafile)
+    main(arguments)
