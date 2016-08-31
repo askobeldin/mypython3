8221a0571842205ff0759d81fa94c4a4cce9f545
askobeldin <askobeldin@gmail.com>
2016-08-31 16:31:47 +0300

    import_1 changed

--- misc/importing/import_1/saa/__init__.py
+++ misc/importing/import_1/saa/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 ################################################################################
-# import sys
-# from os.path import join, dirname
-
-# __path__ = ['utils']
-# __path__.append(join(dirname(__file__), 'utils'))
-# __path__.append('utils')
-
-from .utils import *
-
-# print(__path__)
 
 REGISTRY = {}
 
