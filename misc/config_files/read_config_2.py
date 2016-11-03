b82d9dc954f9c7eee28fcb8bcb0752575841489a
askobeldin <askobeldin@gmail.com>
2016-11-03 16:38:45 +0300

    read_config_2.py added

---
+++ misc/config_files/read_config_2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+################################################################################
+from configparser import ConfigParser, ExtendedInterpolation
+import re
+import myutils
+
+parser = ConfigParser(interpolation=ExtendedInterpolation(), strict=True,
+                      allow_no_value=False)
+parser.SECTCRE = re.compile(r"\[ *(?P<header>[^]]+?) *\]")
+
+with open('config1.cfg', encoding='utf-8') as f:
+    parser.read_file(f)
+    myutils.showconfig1(parser)
