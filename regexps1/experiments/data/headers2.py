6b3e4f739aa6a02a6cf6752a06fa1e084218efbc
Andrey Skobeldin <askobeldin@gmail.com>
2016-12-06 21:36:26 +0300

    small changes

--- regexps1/experiments/data/headers2.py
+++ regexps1/experiments/data/headers2.py
@@ -6,15 +6,20 @@
 # strings
 h1 = '# This is header one.   '
 h11 = '# This is header one.   ###### '
+
 h2 = '#This is header two!!.'
 h21 = '#This is header two #'
+
 h3 = '# This is header, one and two.  '
 h31 = '# This is header, one and two. # '
+
+h4  = 'This is header, level'
 
 
 # regexps
 r1 = r'^#{1}\s*(?P<value>\b.+\b)'
 r2 = r'^#{1}\s*(?P<value>\b.+\b(?:\.|\!|\?)?)'
+r3 = r'^(?P<level>#{1,6})\s*(?P<value>\b.+\b(?:\.|\!|\?)?)'
 
 # main table for testing
 # must be in every file
@@ -36,5 +41,12 @@ retable = (
 
     (r2, h3),
     (r2, h31),
+
+    (r3, '#' + h4 + ' one. #'),
+    (r3, '##  ' + h4 + ' two ###########'),
+    (r3, '###' + h4 + ' three #'),
+    (r3, '####' + h4 + ' four!'),
+    (r3, '#### ' + h4 + ' four!'),
+    (r3, '#### ' + h4 + ' four,'),
 )
 
