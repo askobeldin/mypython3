8fc8041681dfac067e3335827698c058dbe1455f
askobeldin <askobeldin@gmail.com>
2017-03-31 17:30:42 +0300

    modified

--- misc/paths/generator.py
+++ misc/paths/generator.py
@@ -3,30 +3,46 @@
 ################################################################################
 import sys
 import os
+import argparse
 
+
+# folder's name with parts of config
+SUBFOLDER = 'foo'
 
 def mkabspath(*chunks):
     items = list(chunks)
     return os.path.abspath(os.path.join(*items))
 
-def main(curdir):
-    # print(__file__)
-    print('Current dir: {}'.format(curdir))
-    foodir = mkabspath(curdir, 'foo')
-    foofiles = os.listdir(path=foodir)
-    for name in foofiles:
-        apath = mkabspath(foodir, name)
-        if os.path.isfile(apath):
-            print('file: {}'.format(apath))
-    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help="File to read")
+    arguments = parser.parse_args()
+    currentdir = os.path.abspath(os.path.dirname(__file__))
+    filenames = []
+    with open(arguments.file, 'r', encoding='utf-8') as f:
+        filenames = f.readlines()
+    # clean from comment lines
+    filenames = [line.strip() for line in filenames
+                     if not line.lstrip().startswith('#')]
+    # replace names to the full paths
+    filenames = [mkabspath(currentdir, SUBFOLDER, line) for line in filenames]
+    # check existence of files
+    for line in filenames:
+        if not os.path.isfile(line):
+            raise FileNotFoundError('file {} not found!'.format(line))
+    # process files
+    for path in filenames:
+        with open(path, 'r', encoding='utf-8') as f:
+            for textline in f:
+                if not textline.lstrip().startswith('#'):
+                    print(textline, end='')
+
 
 if __name__ == '__main__':
     rc = 1
-    curdir = os.path.abspath(os.path.dirname(__file__))
     try:
-        main(curdir)
+        main()
         rc = 0
     except Exception as e:
         print('Error: %s' % e, file=sys.stderr)
     sys.exit(rc)
-
